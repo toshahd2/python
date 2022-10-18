db992b3442a216c6726ffe405495615f7025dd8e
«Anton» <a@aevip.ru>
2022-10-18 23:41:00 +0500

    hw3 t2 upd

--- hw3/task2/task2.py
+++ hw3/task2/task2.py
@@ -8,7 +8,11 @@ print(f"список:\n{mylist}")
 
 multip = []
 
-for i in range(0, int(len(mylist)/2)):
+if len(mylist)%2 != 0:
+    rang = len(mylist)//2 + 1
+else:
+    rang = len(mylist)//2
+for i in range(0, rang):
     multip.append(mylist[i] * mylist[len(mylist)-1-i])
 
 print(f"произведение пар чисел списка:\n{multip}")
