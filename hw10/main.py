6ebd97653870a18e7c3d8932c2710d2741f36c9f
Anton <tosha.hd2@gmail.com>
2023-03-14 21:18:35 +0300

    test

--- hw10/main.py
+++ hw10/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import telebot
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def get_token():
+    with open('token.txt', 'r') as file:
+        return file.read()
 
+bot = telebot.TeleBot(get_token())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+kboard = telebot.types.InlineKeyboardMarkup()
+kboard.row(     telebot.types.InlineKeyboardButton('Рациональные', callback_data='rational'),
+                telebot.types.InlineKeyboardButton('Комплексные', callback_data='complex'))
+kboard.row(     telebot.types.InlineKeyboardButton('Посмотреть лог', callback_data='log'))
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id,
+    "Привет, " + message.from_user.first_name + "! Это калькулятор рациональных и комплексных чисел!")
+    bot.send_message(message.chat.id, 'Выберите инструмент:', reply_markup=kboard)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "rational":
+        bot.answer_callback_query(call.id, "Рациональные")
+    elif call.data == "complex":
+        bot.answer_callback_query(call.id, "Комплексные")
+    elif call.data == "log":
+        bot.answer_callback_query(call.id, "Посмотреть лог")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling()
